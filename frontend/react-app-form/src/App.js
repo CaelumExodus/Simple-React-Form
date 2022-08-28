87fcf02004b4ae7a2004e7433981a826eed97814
Aleksander <tea418pot@gmail.com>
2022-08-28 18:42:08 +0200

    SimpleReactApp | Squashed commit of the following:
    commit a975ca4488fb78c99102f7a3754bcc9a85b95cd6
    Author: Aleksander <tea418pot@gmail.com>
    Date:   Sun Aug 28 18:41:19 2022 +0200
    
        SimpleReactApp | created frontEnd

--- frontend/react-app-form/src/App.js
+++ frontend/react-app-form/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [firstName, setFName] = useState('');
+  const [lastName, setLName] = useState('');
+  const [email, setEmail] = useState('');
+  const [formDate, setDate] = useState('');
+
+  const displayInfo = () => {
+    console.log(firstName)
+    console.log(lastName)
+    console.log(email)
+    console.log(formDate)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper form-wrapper">
+      <label>
+        First name:
+        <input
+          type="text"
+          onChange={ event => setFName(event.target.value) }
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          type="text"
+          onChange={ event => setLName(event.target.value) }
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          onChange={ event => setEmail(event.target.value) }
+        />
+      </label>
+      <label>
+        Name:
+        <input
+          type="date"
+          onChange={ event => setDate(event.target.value) }
+        />
+      </label>
+      <button
+        className="btn"
+        onClick={ displayInfo }
+      >
+        Send form
+      </button>
     </div>
   );
 }
